54072b367a93fed78802648b476037a6d5a64912
jettro <jettro@gridshore.nl>
2010-03-22 19:59:27 +0100

    Improved the listener for the different contact events

--- src/groovy/nl/gridshore/sample/addressbook/listener/ContactEventListener.groovy
+++ src/groovy/nl/gridshore/sample/addressbook/listener/ContactEventListener.groovy
@@ -2,33 +2,41 @@ package nl.gridshore.sample.addressbook.listener
 
 import nl.gridshore.sample.addressbook.Contact
 import nl.gridshore.sample.addressbook.event.ContactCreatedEvent
-import org.axonframework.core.Event
-import nl.gridshore.sample.addressbook.event.ContactNameChangedEvent
 import nl.gridshore.sample.addressbook.event.ContactDeletedEvent
+import nl.gridshore.sample.addressbook.event.ContactNameChangedEvent
+import org.axonframework.core.Event
 
 /**
  * @author Jettro Coenradie
  */
 class ContactEventListener implements org.axonframework.core.eventhandler.EventListener {
 
-  def ContactEventListener(eventBus) {
-    print "initialize the listener \n"
-    eventBus.subscribe this
-  }
-
-  void handle(Event event) {
-    print "Incoming event \n"
-
-    if (event instanceof ContactCreatedEvent) {
-      def contact = new Contact(name: event.name, identifier:event.aggregateIdentifier.toString())
-      contact.save()
-    } else if (event instanceof ContactNameChangedEvent) {
-      Contact foundContact = Contact.findByIdentifier(event.aggregateIdentifier.toString())
-      foundContact.name = event.newName
-      foundContact.save()
-    } else if (event instanceof ContactDeletedEvent) {
-      Contact foundContact = Contact.findByIdentifier(event.aggregateIdentifier.toString())
-      foundContact.delete()
+    def ContactEventListener(eventBus) {
+        eventBus.subscribe this
     }
-  }
+
+    void handle(Event event) {
+        println "Recevied an event : ${event.class.simpleName}"
+        if (event.class in [ContactCreatedEvent, ContactNameChangedEvent, ContactDeletedEvent]) {
+            doHandle(event)
+        } else {
+            println "Event not supported"
+        }
+    }
+
+    private void doHandle(ContactCreatedEvent event) {
+        def contact = new Contact(name: event.name, identifier: event.aggregateIdentifier.toString())
+        contact.save()
+    }
+
+    private void doHandle(ContactNameChangedEvent event) {
+        Contact foundContact = Contact.findByIdentifier(event.aggregateIdentifier.toString())
+        foundContact.name = event.newName
+        foundContact.save()
+    }
+
+    private void doHandle(ContactDeletedEvent event) {
+        Contact foundContact = Contact.findByIdentifier(event.aggregateIdentifier.toString())
+        foundContact.delete()
+    }
 }
